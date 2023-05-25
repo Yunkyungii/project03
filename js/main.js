8250c682a5e4068a828cee79e936531359b14203
yunkyung <133613667+Yunkyungii@users.noreply.github.com>
2023-05-25 18:10:30 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -27,6 +27,25 @@ $(function () {
 
     });
 
+    $('.awards_content').slick({
+        arrows: false,
+        // fade: true,
+        // vertical: true,
+        autoplay: false,
+        pauseOnHover: false,
+        pauseOnFocus: false,
+    });
+
+    $('.awards .dots li').on('click', function () {
+        //내가 클릭한 요소의 번호를 알려줘...
+        let idx = $(this).index();
+        console.log(idx);
+        $('.awards_content').slick('slickGoTo', idx);
+
+        $(this).addClass('on').siblings().removeClass('on');
+
+    });
+
 
     $(window).on('scroll', function () {
         // 변수에 스크롤한 양을 담는다.
@@ -39,6 +58,8 @@ $(function () {
         }
 
     });
+
+    $('html, body').stop().animate({ scrollTop: '-=300' });
 
 });
 
